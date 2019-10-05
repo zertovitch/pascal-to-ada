40e2419b88ea74a3c70d7164080a2158148bd5ec
Gautier de Montmollin <gautier.demontmollin@deepbluecap.com>
2019-10-05 15:46:50 +0200

    from svn rev 36

--- newp2ada/current/compile.bat
+++ newp2ada/current/compile.bat
@@ -2,15 +2,15 @@ goto debug
 
 :debug
 
-gnatmake -i %1 -aOACU -g -gnatwa -gnato -gnatVa -gnatecdebug.pra p2ada 
-gnatmake -i %1 -aOACU -g -gnatwa -gnato -gnatVa -gnatecdebug.pra bp2p  
+gnatmake -i %1 -aOACU -g -gnatwa -gnato -gnatVa -gnatecdebug.pra  p2ada  -bargs -Es
+gnatmake -i %1 -aOACU -g -gnatwa -gnato -gnatVa -gnatecdebug.pra  bp2p   -bargs -Es
 
 goto end
 
 :release
 
-gnatmake -i %1 -aOACU -g -gnatp -O2 p2ada -largs -s
-gnatmake -i %1 -aOACU -g -gnatp -O2 bp2p  -largs -s
+gnatmake -i %1 -aOACU -g -gnatp -O2  p2ada  -largs -s -bargs -Es
+gnatmake -i %1 -aOACU -g -gnatp -O2  bp2p   -largs -s -bargs -Es
 
 :end
 
